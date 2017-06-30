c6d50ff8dd3c37a635203cff13d8fca33178ba47
kentrino <kentrino@h-kento.jp>
2017-06-30 18:43:15 +0900

    :hammer: (RedisConnection) refactor a bit

--- lib/redis_connection.rb
+++ lib/redis_connection.rb
@@ -1,22 +1,24 @@
 class RedisConnection
-  #include Singleton
   CONFIG = Application::REDIS_CONFIG
   POOL_SIZE = CONFIG[:pool]
-end
 
-connections = []
-round_robin_index = 0
+  class << self
+    def conn
+      init_if_needed
+      @round_robin_index = (@round_robin_index + 1) % POOL_SIZE
+      @connections[@round_robin_index]
+    end
 
-RedisConnection.define_singleton_method :init_conn do
-  RedisConnection::POOL_SIZE.times do |i|
-    connections[i] = Redis.new(RedisConnection::CONFIG)
+    private
+    def init_if_needed
+      @connections ||= []
+      @round_robin_index ||= 0
+      if @connections.size == POOL_SIZE
+        return
+      end
+      POOL_SIZE.times do |i|
+        @connections[i] = Redis.new(CONFIG)
+      end
+    end
   end
-end
-
-RedisConnection.define_singleton_method :conn do
-  if connections.size != RedisConnection::POOL_SIZE
-    init_conn
-  end
-  round_robin_index = (round_robin_index + 1) % RedisConnection::POOL_SIZE
-  connections[round_robin_index]
 end
